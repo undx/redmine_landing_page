453883e16b2139081cead1085068f5ec6537ffc5
Emmanuel GALLOIS <emmanuel.gallois@gmail.com>
2013-03-08 13:25:56 +0100

    Update init.rb to r

--- init.rb
+++ init.rb
@@ -2,15 +2,14 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_landing_page do
   name 'Redmine Landing Page plugin'
-  author 'Igor Zubkov'
+  author 'Igor Zubkov / undx'
   description 'Redmine Landing Page plugin'
-  version '0.0.2'
-  url 'https://github.com/biow0lf/redmine_landing_page'
-  author_url 'https://github.com/biow0lf'
+  version '0.0.3'
+  url 'https://github.com/undx/redmine_landing_page'
+  author_url 'https://github.com/undx'
 end
 
-require 'dispatcher'
-Dispatcher.to_prepare :redmine_contracts do
+Rails.configuration.to_prepare do
   require_dependency 'projects_controller'
   ProjectsController.send(:include, RedmineLandingPage::Patches::ProjectsControllerPatch)
 
